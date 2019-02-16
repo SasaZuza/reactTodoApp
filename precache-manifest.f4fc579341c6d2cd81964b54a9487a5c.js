b81c89055f63eb5288bb604ecc73136801f27de7
‘SasaZuza’ <sasazuzazule@gmail.com>
2019-02-16 14:57:04 +0100

    Updates

---
+++ precache-manifest.f4fc579341c6d2cd81964b54a9487a5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52e0145dab1509cbacd4",
+    "url": "/reactTodoApp/static/js/runtime~main.52e0145d.js"
+  },
+  {
+    "revision": "33021a3cda97eeda4fa5",
+    "url": "/reactTodoApp/static/js/main.33021a3c.chunk.js"
+  },
+  {
+    "revision": "6f96e78ba8e11d79daff",
+    "url": "/reactTodoApp/static/js/1.6f96e78b.chunk.js"
+  },
+  {
+    "revision": "33021a3cda97eeda4fa5",
+    "url": "/reactTodoApp/static/css/main.19f1fc44.chunk.css"
+  },
+  {
+    "revision": "85b69b0eea035270eb3b1e7ccaff1a44",
+    "url": "/reactTodoApp/index.html"
+  }
+];
